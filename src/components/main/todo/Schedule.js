5a6e011449655fa79592b9cda5d0b10f1660412e
황준서 <1201q@naver.com>
2023-09-05 17:11:18 +0900

    [Add] 안되니까 롤백

--- src/components/main/todo/Schedule.js
+++ src/components/main/todo/Schedule.js
@@ -131,7 +131,7 @@ export default function Schedule() {
         <TodoDetailBtn
           onClick={() => {
             setCurrentTab("todo");
-            router.push({ query: { page: "todo" } }, "/");
+            router.push({ query: { page: "todo" } });
           }}
         >
           <Plus width={23} height={23} fill={colors.font.gray} />
